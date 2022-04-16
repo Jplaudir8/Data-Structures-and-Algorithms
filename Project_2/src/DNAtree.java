fdeaace503a0450615308a0e4d9fa16786b2e32a
Joan-Perez-Lozano <joanperezl123@gmail.com>
2022-04-16 17:15:20 -0400

    added Command class

--- Project_2/src/DNAtree.java
+++ Project_2/src/DNAtree.java
@@ -67,7 +67,6 @@ public class DNAtree {
             }
         }
         scannedFile.close();
-
     }
 
 
--- Project_4/src/Rectangle1.java
+++ Project_4/src/Rectangle1.java
@@ -1,9 +1,72 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
+//On my honor:
+//- I have not used source code obtained from another student,
+//or any other unauthorized source, either modified or
+//unmodified.
+//
+//- All source code and documentation used in my program is
+//either my original work, or was derived by me from the
+//source code published in the textbook for this course.
+//
+//- I have not discussed coding details about this project
+//with anyone other than my partner (in the case of a joint
+//submission), instructor, ACM/UPE tutors or the TAs assigned
+//to this course. I understand that I may discuss the concepts
+//of this program with other students, and that another student
+//may help me debug my program so long as neither of us writes
+//anything during the discussion or modifies any computer file
+//during the discussion. I have violated neither the spirit nor
+//letter of this restriction.
+
+/**
+ * The Rectangle class represents a Skip List data structure that stores...(to elaborate)
+ *
+ * @author Joan Piayet Perez Lozano (joanperezl)
+ * @author Raena Rahimi Bafrani (raenar)
+ * @version Apr 16, 2022
+ */
 public class Rectangle1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Read commands
+		Scanner scannedFile = scanFile(args[0]);
+		while (scannedFile.hasNext()) {
+			String line = trimWhitespaceOf(scannedFile.nextLine());
+			if (!line.isEmpty()) {
+				Command currCommand = new Command(line);
+				currCommand.executeCommandOperation();
+			}
+		}
+		scannedFile.close();
 	}
-
+	
+	
+	/**
+	 * Trims leading and trailing whitespace. And also extra space that might be present in between the commands.
+	 * @param lineRead
+	 * @return
+	 */
+	private static String trimWhitespaceOf(String lineRead) {
+		String cleanedLine = lineRead.trim().replaceAll("\\s+", " ");
+		return cleanedLine;
+	}
+	
+	
+	/**
+	 * Scans the file to be read
+	 * @param filePath path to the file to be read
+	 * @return scanned file as a scanner
+	 */
+    public static Scanner scanFile(String filePath) {    
+    	try {
+    		FileReader file = new FileReader(filePath);
+    		return new Scanner(file);
+        } catch(FileNotFoundException e) {
+        	e.printStackTrace();
+        }
+        return null;
+    }
 }
